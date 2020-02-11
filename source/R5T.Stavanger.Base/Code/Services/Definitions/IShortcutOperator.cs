e3ec6cf6be68a4a7252c22245111a6008efa7251
MinexAutomation <minex.automation@gmail.com>
2020-02-11 00:51:09 -0800

    Added IShortcutOperator service definition.

---
+++ source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Stavanger
+{
+    public interface IShortcutOperator
+    {
+        string GetShortcutTargetPath(string shortcutFilePath);
+        void CreateShortcut(string shortcutFilePath, string shortcutTargetPath);
+    }
+}
