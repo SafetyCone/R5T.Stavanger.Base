fbfda9035796c87d5b659ae08d1aeb5a0b4e4564
MinexAutomation <minex.automation@gmail.com>
2020-02-11 01:05:57 -0800

    Added IShortcutPathConventions extensions.

---
+++ source/R5T.Stavanger.Base/Code/Services/Extensions/IShortcutPathConventionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace R5T.Stavanger
+{
+    public static class IShortcutPathConventionsExtensions
+    {
+        /// <summary>
+        /// Adds the link suffix to the file path.
+        /// </summary>
+        /// <remarks>
+        /// A link will only display its file name and file extension in Windows Explorer, not the invisible link suffix. This means that to actually get the path of a link file, you have to add the invisible link suffix.
+        /// </remarks>
+        public static string MakeFilePathIntoLinkFilePath(this IShortcutPathConventions shortcutPathConventions, string filePath)
+        {
+            var linkSuffixExtensionSeparator = shortcutPathConventions.GetLinkSuffixExtensionSeparator();
+            var linkSuffixExtension = shortcutPathConventions.GetLinkSuffixExtension();
+
+            string output = $"{filePath}{linkSuffixExtensionSeparator}{linkSuffixExtension}";
+            return output;
+        }
+    }
+}
