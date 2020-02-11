e792af1e749193111f0b264887488012101144bb
MinexAutomation <minex.automation@gmail.com>
2020-02-11 01:05:32 -0800

    Added method output parameter. Added documentation.

--- source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
+++ source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
@@ -6,6 +6,9 @@ namespace R5T.Stavanger
     public interface IShortcutOperator
     {
         string GetShortcutTargetPath(string shortcutFilePath);
-        void CreateShortcut(string shortcutFilePath, string shortcutTargetPath);
+        /// <summary>
+        /// Outputs the adjusted shortcut file path (perhaps including the ".lnk" suffix).
+        /// </summary>
+        string CreateShortcut(string shortcutFilePath, string shortcutTargetPath);
     }
 }
