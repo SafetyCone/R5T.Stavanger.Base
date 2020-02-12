357aa46188ffbe90c4429538bc0f24ff44725728
MinexAutomation <minex.automation@gmail.com>
2020-02-11 21:25:12 -0800

    Improved IShortcutOperator service definition with methods for getting/setting the shortcut description (file-name?).

--- source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
+++ source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
@@ -6,9 +6,16 @@ namespace R5T.Stavanger
     public interface IShortcutOperator
     {
         string GetShortcutTargetPath(string shortcutFilePath);
+
+        void SetShortcutTargetPath(string shortcutFilePath, string shortcutTargetPath);
+
         /// <summary>
         /// Outputs the adjusted shortcut file path (perhaps including the ".lnk" suffix).
         /// </summary>
-        string CreateShortcut(string shortcutFilePath, string shortcutTargetPath);
+        string CreateShortcut(string shortcutFilePath, string shortcutTargetPath, string description);
+
+        string GetShortcutDescription(string shortcutFilePath);
+
+        void SetShortcutDescription(string shortcutFilePath, string description);
     }
 }
