b8ee2958dd51f6cb247a6f14cd55fdf3d4a2a817
MinexAutomation <minex.automation@gmail.com>
2020-02-11 22:21:03 -0800

    Added IShortcutOperator extensions.

---
+++ source/R5T.Stavanger.Base/Code/Services/Extensions/IShortcutOperatorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Stavanger
+{
+    public static class IShortcutOperatorExtensions
+    {
+        public static string CreateShortcut(this IShortcutOperator shortcutOperator, string shortcutFilePath, string shortcutTargetPath)
+        {
+            var description = String.Empty;
+
+            var shortcutLinkFilePath = shortcutOperator.CreateShortcut(shortcutFilePath, shortcutTargetPath, description);
+            return shortcutLinkFilePath;
+        }
+    }
+}
