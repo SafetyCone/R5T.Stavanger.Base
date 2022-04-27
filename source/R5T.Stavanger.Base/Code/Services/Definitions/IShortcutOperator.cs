4a1e2037b60fab0ebff8a96947352720680185ea
davidcoats <david@getsafetycone.com>
2022-04-27 15:35:43 -0700

    Interim changes.

--- source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
+++ source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutOperator.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Stavanger
-{
-    public interface IShortcutOperator
+{[ServiceDefinitionMarker]
+    public interface IShortcutOperator:IServiceDefinition
     {
         string GetShortcutTargetPath(string shortcutFilePath);
 
--- source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutPathConventions.cs
+++ source/R5T.Stavanger.Base/Code/Services/Definitions/IShortcutPathConventions.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Stavanger
-{
-    public interface IShortcutPathConventions
+{[ServiceDefinitionMarker]
+    public interface IShortcutPathConventions:IServiceDefinition
     {
         string GetLinkSuffixExtensionSeparator();
         string GetLinkSuffixExtension();
